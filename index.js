cedd8c830ca3010eff9ecf049b2288f1383ddefa
Igor Borysyuk <borysyuk@qarea.com>
2015-10-27 14:16:06 +0200

    superagent has been replaced by isomorphic-fetch

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var _ = require('underscore');
 var csv = require('csv');
 var Promise = require('bluebird');
-var request = require('superagent');
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
 var config = {
   backend: 'https://rawgit.com/dataprotocols/registry/master/registry.csv',
@@ -10,46 +11,37 @@ var config = {
 
 
 function getCSVEndpoint(endpoint, objectwise) {
-
   /**
    * Return data from an endpoint that is parsable as CSV
    */
 
   return new Promise(function(resolve, reject) {
-
-    request
-      .get(endpoint)
-      .end(function(error, response) {
-
-        if (error)
-          reject('Failed to download registry file: ' + error);
-
-        csv.parse(response.text, {columns: objectwise}, function(error, output) {
-
-          if (error)
-            reject('Failed to parse registry file: ' + error);
-
-          resolve(output);
-
+    fetch(endpoint).
+        then(function(response) {
+          if (response.status != 200) {
+            reject("Bad response from server");
+          }
+          return response.text();
+        }).then(function(text){
+          csv.parse(text, {columns: objectwise}, function(error, output) {
+            if (error) {
+              reject('Failed to parse registry file: ' + error);
+            }
+            resolve(output);
+          });
         });
-      });
-
   });
 }
 
 
 function getRegistry(userConfig) {
-
   /**
    * Return the DataPackage Registry as an array of objects
    */
   
   // Replace default params with user config
   var customConfig = _.extend(config, userConfig);
-
-
   return getCSVEndpoint(customConfig.backend, customConfig.objectwise);
-
 }
 
 
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var _ = require('underscore');
-var request = require('superagent');
+var fetchMock = require('fetch-mock');
 var assert = require('chai').assert;
 var Promise = require('bluebird');
 var registry = require('./');
@@ -7,28 +7,26 @@ var should = require('chai').should();
 
 
 describe('Data Package Registry', function(){
-  var requestConfig = {
-    callback: function (match, data) { return {text: data}; },
-    fixtures: function() { return ''; },
-
-    // datapackage-registry doesn't export registry URL
-    pattern: '.*'
+  var config = {
+    backend: 'https://rawgit.com/dataprotocols/registry/master/registry.csv',
   };
 
   describe('get()', function(){
     it('return Promise object', function(done, err){
       if (err) done(err);
-      require('superagent-mock')(request, [requestConfig]);
+      fetchMock.restore();
+      fetchMock.mock(config.backend, 'id,title,schema,specification');
+
       registry.get().should.be.an.instanceOf(Promise);
+
       done();
     });
 
     it('resolve into non-empty array of objects when registry is not empty', function(done, err){
       if (err) done(err);
 
-      require('superagent-mock')(request, [_.extend(requestConfig, {
-        fixtures: function(){ return 'id,title,schema,specification\n1,2,3,4'; }
-      })]);
+      fetchMock.restore();
+      fetchMock.mock(config.backend, 'id,title,schema,specification\n1,2,3,4');
 
       registry.get().then(function(data) {
         data.should.be.not.empty;
@@ -39,31 +37,20 @@ describe('Data Package Registry', function(){
     it('resolve into empty array when registry is empty', function(done, err){
       if (err) done(err);
 
-      require('superagent-mock')(request, [_.extend(requestConfig, {
-        fixtures: function(){ return ' '; }
-      })]);
+      fetchMock.restore();
+      fetchMock.mock(config.backend, 'id,title,schema,specification');
 
       registry.get().then(function(data) {
         data.should.be.empty;
-
-        require('superagent-mock')(request, [_.extend(requestConfig, {
-          fixtures: function(){ return 'id,title,schema,specification'; }
-        })]);
-
-        // Feel lack of experience in async testing
-        registry.get().then(function(data) {
-          data.should.be.empty;
-          done();
-        });
+        done();
       });
     });
 
     it('reject with a message when connection failed', function(done, err){
       if (err) done(err);
 
-      require('superagent-mock')(request, [_.extend(requestConfig, {
-        callback: function(){ throw new Error(500); }
-      })]);
+      fetchMock.restore();
+      fetchMock.mock(config.backend, 500);
 
       registry.get().catch(function(error) {
         error.should.be.a('string');
